a124addc57f52aefa0eb8a8dc3a8945919411797
Victor Mello <victorsmello93@gmail.com>
2020-10-21 22:56:22 -0300

    Aula 1 Bootcamp ASP.NET Core - Construindo uma API para gerenciamento de academias de ginástica
    -Montada a estrutura de pastas e módulos
    -Montada a interface da API com Swagger
    -Construídos os endpoints (rotas) da API
    -Adicionadas as classes de entidades, enums e um controller para os testes no funcionamento da Swagger

---
+++ AwesomeGym.API/Controllers/AlunosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeGym.API.Controllers
+{
+    [ApiController]
+    [Route("api/alunos")]
+    public class AlunosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
